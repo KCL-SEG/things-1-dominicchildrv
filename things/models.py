a0b80ee8ca464bb58435ad60f823c64f989f4dc3
Dominic Child <dominicchild09@yahoo.com>
2022-10-23 06:47:39 +0100

    implement exercise 3

--- things/models.py
+++ things/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=120, unique=False)
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, unique=False, blank=True)
+    quantity = models.IntegerField(unique=False, max_value=100, min_value=0)
