ec9fdd443e85e871785ccd6266664ac3308f6b25
Dominic Child <dominicchild09@yahoo.com>
2022-10-23 06:52:35 +0100

    fix issue with code

--- things/models.py
+++ things/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
