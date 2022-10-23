86726371359d619a59261b606273f0b3971afbe9
Dominic Child <dominicchild09@yahoo.com>
2022-10-23 06:58:02 +0100

    add missing =

--- things/models.py
+++ things/models.py
@@ -5,7 +5,7 @@ class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False,
-        validators[
+        validators=[
             MaxValueValidator(100),
             MinValueValidator(0)
         ]
